ae285d1524245aaba35c7e0aa4e39e206c5a1785
Oz Browning <56755170+ozmodeuz@users.noreply.github.com>
2024-12-24 16:06:52 +0000

    start of command/subcommand structure

---
+++ commands.md
@@ -0,0 +1,45 @@
+# init
+Create a default sysenv config and initialize it as a git repo.
+
+# add
+Add packages and options.
+add [-p] name [-t]
+-p, --package
+-t, --tag
+
+# remove
+Remove packages and options.
+
+# search
+Search for packages and options.
+-p, --package
+-o, --option
+-i, --inputs
+
+# update
+Update all options and packages.
+
+# clean
+Remove old config generations.
+-a, --age
+
+# info
+Print information about the system.
+
+# input
+Add an input.
+
+# help
+Print this message, or the help of the given subcommands.
+
+# version
+Print the version of sysenv.
+
+# common arguments
+--verbose
+--quiet
+--max-jobs
+--impure
+--cache
+--refresh-cache
+--offline
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
-use std::fs;
-use nixel::parse;
-use serde::Serialize;
+use clap::{Command, Arg};
+
+fn init() {}
+
+fn app() -> Command {
+    Command::new("sysenv").about("A tool for managing system environment configurations.")
+        .subcommand(Command::new("init")
+            .about("")
+            .aliases(["i", "initialize", "setup", "s"]))
+        .subcommand(Command::new("add")
+            .about("")
+            .aliases(["a", "install"]))
+        .subcommand(Command::new("remove")
+            .about("")
+            .aliases(["rm", "delete", "d", "uninstall", "un"]))
+        .subcommand(Command::new("search")
+            .about("")
+            .aliases(["s", "find", "f", "query", "q"]))
+        .subcommand(Command::new("update")
+            .about("")
+            .aliases(["u", "upgrade"])) 
+        .subcommand(Command::new("clean")
+            .about("")
+            .aliases(["c", "gc"]))
+        .subcommand(Command::new("show")
+            .about("")
+            .aliases(["s", "info"]))
+        .subcommand(Command::new("input")
+            .about(""))
+        .subcommand(Command::new("help").about(""))
+        .subcommand(Command::new("version").about(""))
+}
 
 fn main() {
-    let input = fs::read_to_string("/home/oz/.nix/flake.nix").unwrap();
-    let parsed = parse(input);
-    
-    // Create a JSON serializer
-    let mut serializer = serde_json::Serializer::new(std::io::stdout());
-    
-    // Serialize the parsed data
-    println!("{:#?}", parsed.serialize(&mut serializer).unwrap());
+    let app = app();
 }
