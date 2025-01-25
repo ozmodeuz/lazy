9bfd63d3c10b47c874aa7e2e01636d60e45ea1fc
Oz Browning <56755170+ozmodeuz@users.noreply.github.com>
2025-01-25 12:04:22 +0000

    tidy up

--- nix/rebuild.sh
+++ nix/rebuild.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname $0)
 
-nixpkgs=$(nix eval --raw -f ${PWD}/sysconf.nix nixpkgs)
-nix_path="nixpkgs=${nixpkgs}:nixos-config=${PWD}/sysconf.nix"
+nixpkgs=$(nix eval --raw -f ${PWD}/lazy.nix nixpkgs)
+nix_path="nixpkgs=${nixpkgs}:nixos-config=${PWD}/lazy.nix"
 
 sudo env NIX_PATH="${nix_path}" nixos-rebuild build-vm --fast "$@"
